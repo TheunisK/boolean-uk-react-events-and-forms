8674463acb0ce86065633ba0896527333372bfcb
Theunis Knoetze <theunis.knoetze@gmail.com>
2021-12-08 15:45:12 +0300

    booking complete

--- src/sections/Booking/index.js
+++ src/sections/Booking/index.js
@@ -3,21 +3,45 @@ import { useState } from "react"
 function BookingSection() {
   const [formState, setFormState] = useState({
     date: "",
-    soakAndSwim: false,
+    pool: false,
     facial: false,
     massage: false,
-    dailyNewspaper: false,
-    thing: false
+    newspaper: false,
+    breakfast: false
   });
+
+  const handleForm = event => {
+    const inputName = event.target.name;
+    const inputType = event.target.type;
+    let inputValue = event.target.checked;
+    if (inputType === "datetime-local") {
+      inputValue = event.target.value
+    }
+    setFormState({...formState, [inputName]: inputValue})
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(formState)
+    setFormState({
+      date: "",
+      pool: false,
+      facial: false,
+      massage: false,
+      newspaper: false,
+      breakfast: false
+    })
+  }
 
   return (
     <section className="shadow pad-lg">
       <h2>Booking Form</h2>
-      <form className="form-stack">
+      <form className="form-stack" onSubmit={handleSubmit}>
         <label htmlFor="date">When will you be arriving?</label>
         <input 
-          onChange={e => console.log(e)}
-          type="datetime-local" 
+          onChange={handleForm}
+          type="datetime-local"
+          name="date"
         />
         <section>
           <h3>Spa Activities</h3>
@@ -25,18 +49,21 @@ function BookingSection() {
             type="checkbox" 
             id="pool" 
             name="pool" 
+            onChange={handleForm}
           />
           <label htmlFor="pool">Soak and Swim</label>
           <input 
             type="checkbox" 
             id="facial" 
             name="facial"
+            onChange={handleForm}
           />
           <label htmlFor="facial">Facial</label>
           <input 
             type="checkbox" 
             id="massage" 
             name="massage" 
+            onChange={handleForm}
           />
           <label htmlFor="massage">Massage</label>
         </section>
@@ -46,12 +73,14 @@ function BookingSection() {
             type="checkbox" 
             id="newspaper" 
             name="newspaper" 
+            onChange={handleForm}
           />
           <label htmlFor="newspaper">Daily Newspaper</label>
           <input 
             type="checkbox" 
             id="breakfast" 
             name="breakfast"
+            onChange={handleForm}
           />
           <label htmlFor="breakfast">Breakfast</label>
         </section>
